b226363a5edb5534e6111a3aa62387410aafbc94
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2024-03-16 23:43:55 +0000

    Added Projects folder

---
+++ projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
@@ -0,0 +1,12 @@
+// Put your Robot and Humanoid classes here! ✨
+// See ./original.js for their older JavaScript code.
+class Robot {
+	constructor(name, abilities) {
+		this.name = name;
+		this.abilities = abilities;
+		this.power = 100;
+	}
+}
+
+module.exports.Humanoid = Humanoid;
+module.exports.Robot = Robot;
